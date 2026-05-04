5d8769184b8e4606e883c9b4546302bdb1901558
lucky gill <gilllucky95@gmail.com>
2026-05-04 16:13:24 +0530

    changes for testing another request url

--- api/epay-select.js
+++ api/epay-select.js
@@ -96,6 +96,7 @@ export default async function handler(req, res) {
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).end();
 
+  console.log("re_header--", req.headers.origin);
   try {
     /* ================== INPUT ================== */
     const {

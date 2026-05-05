cc4709325f3020eae24ee8dfbab48bc3c9f8e534
178883795 <gilllucky95@gmail.com>
2026-05-05 14:28:45 +0530

    remove console

--- api/epay-select.js
+++ api/epay-select.js
@@ -97,7 +97,7 @@ export default async function handler(req, res) {
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).end();
 
-  console.log("re_header--00", req.headers.origin);
+  // console.log("re_header--00", req.headers.origin);
   //  request_URL = req.headers.origin.split("//")[1];
    
   try {
